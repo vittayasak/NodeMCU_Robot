076dfcfe5fcbafb492846a119951cbb1c94ee392
vittayasak <vittayasak@gmail.com>
2015-08-30 23:47:17 +0700

    Create Robot_UDP.ino

---
+++ Robot_UDP.ino
@@ -0,0 +1,219 @@
+/* ############################################################ 
+ *  By: Vittaysak Rujivorakul
+ *  Android App for Controller:  https://play.google.com/store/apps/details?id=com.br3.udpctl&hl=th
+ *  My Demo Clip VDO: https://www.youtube.com/watch?v=E85RfNlRmHU 
+ *  Hardware Micro Controller: NodeMCU V2 (ESP8266-12) found at http://www.nodemcu.com/
+ *  Enjoy your kids
+ *########################################################### */
+
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+ const char* ssid = "WIFI_SSID";
+ const char* password = "WIFI_PASSWORD";
+ const char* host = "IP your computer";
+
+ char message[2];
+
+ unsigned int localPort = 8080;
+ WiFiUDP Udp;
+
+ int pin1_motor_R = 1;   ///E2
+ int pin2_motor_R = 2;
+ int pin1_motor_L = 3;   // E1
+ int pin2_motor_L = 4;
+ 
+int FW=0,BK=1,ST=2;
+int dir=FW;
+
+ void setup() {
+   Serial.begin(115200);
+   pinMode(pin1_motor_R , OUTPUT);
+   pinMode(pin2_motor_R, OUTPUT);
+   pinMode(pin1_motor_L, OUTPUT);
+   pinMode(pin2_motor_L, OUTPUT);
+
+   WiFi.begin(ssid, password);
+   while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+   }
+   Serial.println("");
+   Serial.println("WiFi connected");
+   Serial.println("IP address: ");
+   Serial.println(WiFi.localIP());
+   Udp.begin(localPort);
+   
+   Stop();
+   delay(10);
+   Serial.println("Setup done");
+   delay(2000);
+
+ }
+
+ void loop(){
+   int packetSize = Udp.parsePacket();
+   if (packetSize)
+   {
+     IPAddress remoteIp = Udp.remoteIP();
+     int len = Udp.read(message, 255);
+     Serial.println(message);
+     checkcommand();
+     message[0]=0;
+     message[1]=0;
+     delay(5);
+   }
+} 
+ void forward()
+ {
+   delay(10);
+   Serial.println("forward");
+   
+   dir=FW;
+   digitalWrite(pin1_motor_R, HIGH);
+   digitalWrite(pin2_motor_R, LOW);
+   
+   digitalWrite(pin1_motor_L, HIGH);
+   digitalWrite(pin2_motor_L, LOW);
+   
+ }
+
+
+ void back()
+ {
+   delay(10);
+   Serial.println("back");
+   dir=BK;
+   digitalWrite(pin1_motor_R, LOW);
+   digitalWrite(pin2_motor_R, HIGH);
+   
+   digitalWrite(pin1_motor_L, LOW);
+   digitalWrite(pin2_motor_L, HIGH);
+ }
+
+ void left()
+ {
+  delay(10);
+  Serial.println("left");
+  digitalWrite(pin1_motor_R, LOW);
+  digitalWrite(pin2_motor_R, LOW);
+   
+   digitalWrite(pin1_motor_L, HIGH);
+   digitalWrite(pin2_motor_L, LOW);
+ }
+ void right()
+ {
+  delay(10);
+  Serial.println("Right");
+   digitalWrite(pin1_motor_R, HIGH);
+  digitalWrite(pin2_motor_R, LOW);
+   
+   digitalWrite(pin1_motor_L, LOW);
+   digitalWrite(pin2_motor_L, LOW);
+ }
+
+
+void turnR()
+ {
+  delay(10);
+  Serial.println("Turn0");
+   digitalWrite(pin1_motor_R, HIGH);
+  digitalWrite(pin2_motor_R, LOW);
+   
+   digitalWrite(pin1_motor_L, LOW);
+   digitalWrite(pin2_motor_L, HIGH);
+ }
+ void turnL()
+ {
+  delay(10);
+  Serial.println("Turn1");
+   digitalWrite(pin1_motor_R, LOW);
+  digitalWrite(pin2_motor_R, HIGH);
+   
+   digitalWrite(pin1_motor_L, HIGH);
+   digitalWrite(pin2_motor_L, LOW);
+ }
+
+ void Stop()
+ {
+  dir=ST;
+  delay(10);
+  Serial.println("Stop");
+  
+  digitalWrite(pin1_motor_R, LOW);
+  digitalWrite(pin2_motor_R, LOW);
+   
+   digitalWrite(pin1_motor_L, LOW);
+   digitalWrite(pin2_motor_L, LOW);
+  
+ }
+
+ void next(){
+            if(dir==FW){
+              forward();
+            }else if(dir==BK){
+               back();
+            }else{
+              Stop(); 
+            }
+ }
+
+ void checkcommand()
+ {
+   //Control DC Motor
+   int cm = atoi(message);
+   switch(cm){
+    //UP
+    case 1:                                       break;
+    
+    //DOWN
+    case 2:                                       break;
+
+    //LEFT-90
+    case 3:  left();    delay((dir==ST)?480:580);   next();     break;
+
+    //RIGH-90
+    case 4:  right();   delay((dir==ST)?480:580);   next();     break;
+
+    //STOP
+    case 5:  Stop();                              break;
+
+   //U-Tern 180
+    case 6: turnL();  delay((dir==ST)?480:620);   next();       break;
+
+   //Turn-Left
+    case 7: turnL();                              break;
+
+   //Turn-Righ
+    case 8:   turnR();                            break;
+
+   //FW
+    case 9:   forward();                          break;
+
+    //STEP-FW
+    case 10:  forward();  delay(50);  Stop();     break;
+
+   //STEP-LEFT
+    case 11: left();  delay(50);  Stop();         break;
+
+   //STEP-RIGH
+    case 12: right(); delay(50); Stop();          break;
+
+   //BACK
+    case 13: back();                              break;
+
+   //STEP-BACK
+    case 14: back(); delay(50); Stop();           break;
+
+   //LEFT
+    case 15: left(); delay(200);  next();         break;
+
+   //RIGH
+    case 16: right(); delay(200); next();         break;
+
+    default: Stop();                              break;
+    
+   }
+
+ }
+
